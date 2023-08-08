e2eb6b1b08f29ae8f4973d3969a5428b9d01bfd9
Ilias Farrakhov <iliasuest@gmail.com>
2023-08-08 16:22:31 +0500

    make a task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean2);
+        System.out.println(firstCat == secondCat);
     }
 }
